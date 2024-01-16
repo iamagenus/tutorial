f85b465126ed55b089af67ec86e8a1e5dcfd6844
Iamagenus <brandonlewandowski@icloud.com>
2024-01-16 13:44:48 -0500

    updated config and json files for Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-    site: "https://iamagenusastrotut.netlify.app/"
+  site: "https://iamagenusastrotut.netlify.app/",
+  integrations: [preact()]
 });
